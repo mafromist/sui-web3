144aaae9663523e30def3720e639b5bc1c0b2fc9
mugealev-eva <mafromist@gmail.com>
2025-11-27 23:36:59 +0300

    remove pkg id

--- module_4/src/networkConfig.ts
+++ module_4/src/networkConfig.ts
@@ -12,7 +12,7 @@ const { networkConfig, useNetworkVariable, useNetworkVariables } =
     testnet: {
       url: getFullnodeUrl("testnet"),
       variables: {
-        packageId: "0x557e8be6f572200454b597f429d93b86cc1f4d617edde53adaf3dc707edf3b66", // TODO: Get package ID from instructor
+        packageId: "0x[INSTRUCTOR_PROVIDED_PACKAGE_ID]", // TODO: Get package ID from instructor
       },
     },
     mainnet: {
